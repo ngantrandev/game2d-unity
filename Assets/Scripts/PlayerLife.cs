867ad723431ab6e9d733aecb3abff85bdd8b5666
Ngan Tran <78363797+ngantrandev@users.noreply.github.com>
2023-11-29 00:13:25 +0700

    completed video 8#
    add player death logic

---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Animator animation;
+    private Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        animation = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Trap"))
+        {
+            ShowDeathAnimation();
+        }
+    }
+
+    private void ShowDeathAnimation()
+    {
+        rb.bodyType = RigidbodyType2D.Static;
+        animation.SetTrigger("death");
+    }
+
+    private void RestartLevel()
+    {
+       SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
