01051a00d06f73ee097ce54fb4ce6c8a392083ec
Ngan Tran <78363797+ngantrandev@users.noreply.github.com>
2023-12-25 15:38:35 +0700

    them logic checkpoint, respawn va restart level

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -3,16 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerLife : MonoBehaviour
 {
     [SerializeField] private AudioSource deadthSoundEffect;
+    [SerializeField] private AudioSource checkpointSound;
     private Animator animator;
     private Rigidbody2D rb;
     private SpriteRenderer spriteRenderer;
     private Transform transform;
+    [SerializeField] private Text heartText;
+    [SerializeField] private int maxHeart = 3; // so lan chet toi da
+    private int deathCount = 0; // so lan chet
 
-    Vector2 lastPos;
+    private bool batTu = false;
+
+    Vector2 lastPos, startPos;
 
 
     // Start is called before the first frame update
@@ -23,23 +30,59 @@ public class PlayerLife : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
         transform = GetComponent<Transform>();
 
-        lastPos = transform.position;
+        if (heartText != null)
+        {
+            heartText.text = "Heart: " + maxHeart;
+        }
+
+        startPos = transform.position;
+        lastPos = startPos;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag("Trap"))
+        if (collision.gameObject.CompareTag("Trap") && !batTu)
         {
             deadthSoundEffect.Play();
             ShowDeathAnimation();
+
+            Debug.Log("collide");
+            deathCount++;
+            batTu = true; // loai bo va cham voi vat khac truoc khi game restart hoac respawn
+
+            if (heartText != null && deathCount <= maxHeart)
+            {
+                heartText.text = "Heart: " + (maxHeart - deathCount);
+            }
+
+
+            if (deathCount >= maxHeart)
+            {
+                Debug.Log("Restart");
+                StartCoroutine(RestartLevelAfterDelay(1f));
+            }
+            else
+            {
+                Debug.Log("respawn");
+                StartCoroutine(RespawnAfterDelay(1f)); // delay khong chan luong chinh cua game
+
+            }
+
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log("On trigger");
         if (collision.gameObject.CompareTag("FinishLevel"))
         {
             ShowDisappearAnimation();
+        }
+        else if (collision.gameObject.CompareTag("Checkpoint"))
+        {
+            checkpointSound.Play();
+            collision.gameObject.SetActive(false);
+            lastPos = collision.transform.position;
         }
     }
 
@@ -55,14 +98,25 @@ public class PlayerLife : MonoBehaviour
         animator.SetTrigger("death");
     }
 
-    private void Respawn()
+    private IEnumerator RespawnAfterDelay(float delay)
     {
+
+        yield return new WaitForSeconds(delay);
+
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        transform.position = lastPos;
+        animator.SetTrigger("respawn");
+        batTu = false;
 
     }
 
-    private void RestartLevel()
+    private IEnumerator RestartLevelAfterDelay(float delay)
     {
+        yield return new WaitForSeconds(delay);
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        transform.position = lastPos;
+        transform.position = startPos;
+        animator.SetTrigger("respawn");
+        batTu = false;
     }
 }
